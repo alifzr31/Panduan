16d22a36096e6a3bb2a35e7267fabbc41364572f
Alif ZR <alifzakya.r@gmail.com>
2025-09-16 15:26:09 +0700

    add accessToken and refreshToken name

--- lib/app/utils/app_strings.dart
+++ lib/app/utils/app_strings.dart
@@ -1,5 +1,7 @@
 class AppStrings {
   static const String assetsImages = 'assets/images';
   static const String assetsLotties = 'assets/lotties';
+  static const String accessToken = 'accessToken';
+  static const String refreshToken = 'refreshToken';
   static const String errorApiMessage = 'Ups sepertinya terjadi kesalahan';
 }
