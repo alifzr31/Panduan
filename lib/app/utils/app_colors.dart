e2f420144af9d57e3f11dcc38e96457dae42bc86
Alif ZR <alifzakya.r@gmail.com>
2025-09-16 13:12:25 +0700

    Remove duplicate greyFormField color definition in AppColors for improved clarity

--- lib/app/utils/app_colors.dart
+++ lib/app/utils/app_colors.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color backgroundColor = Color(0xFFF3F3F3);
+  static const Color greyFormField = Color(0xFFE3E6E8);
+  static const Color primaryColor = Color(0xFF8055C8);
   static const Color pinkColor = Color(0xFFFC66C2);
   static const Color greenColor = Color(0xFF7FD758);
   static const Color blueColor = Color(0xFF5271FE);
   static const Color amberColor = Color(0xFFFE914C);
   static const Color purpleColor = Color(0xFF9901CB);
-  static const Color greyFormField = Color(0xFFE3E6E8);
 }
