aefa11661f369bb8afdfd80acfca8fcd18edbe9d
Alif ZR <alifzakya.r@gmail.com>
2025-10-27 15:51:27 +0700

    create base file source bottomsheet

---
+++ lib/app/widgets/show_filesourcebottomsheet.dart
@@ -0,0 +1,128 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:panduan/app/utils/app_colors.dart';
+import 'package:panduan/app/widgets/show_custombottomsheet.dart';
+
+Future<Object?> showFileSourceBottomSheet(BuildContext context) {
+  return showDynamicHeightBottomSheet(
+    context,
+    child: Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'Pilih Sumber File/Gambar',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: CupertinoButton(
+                  minimumSize: Size.zero,
+                  padding: const EdgeInsets.all(2),
+                  onPressed: () async {
+                    final ImagePicker picker = ImagePicker();
+
+                    final pickedFile = await picker.pickImage(
+                      source: ImageSource.camera,
+                      imageQuality: 60,
+                    );
+
+                    if (pickedFile != null) {
+                      if (context.mounted) {
+                        Navigator.pop(context, {
+                          'fileName': pickedFile.name,
+                          'filePath': pickedFile.path,
+                        });
+                      }
+                    }
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          color: AppColors.softBlueColor,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Center(
+                          child: Icon(
+                            MingCute.camera_2_fill,
+                            size: 34,
+                            color: AppColors.blueColor,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        'Kamera',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: CupertinoButton(
+                  minimumSize: Size.zero,
+                  padding: const EdgeInsets.all(2),
+                  onPressed: () async {
+                    final pickedFile = await FilePicker.platform.pickFiles(
+                      type: FileType.custom,
+                      allowedExtensions: ['jpg', 'jpeg', 'png', 'pdf'],
+                    );
+
+                    if (pickedFile != null) {
+                      if (context.mounted) {
+                        Navigator.pop(context, {
+                          'fileName': pickedFile.files.single.name,
+                          'filePath': pickedFile.files.single.path,
+                        });
+                      }
+                    }
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          color: AppColors.softBlueColor,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Center(
+                          child: Icon(
+                            MingCute.folder_2_fill,
+                            size: 34,
+                            color: AppColors.blueColor,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        'File Saya',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
